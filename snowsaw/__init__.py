b311c418db382b06fa66263e78634a7bf17f66dc
Arctic Ice Studio <development@arcticicestudio.com>
2019-06-22 15:27:41 +0200

    Initial repository clean up for Go rewrite
    In order to start the Go project rewrite (1) from scratch the current
    repository structure and files have been reset to a clean state to
    remove all references to the previous implementations, documentations
    and project structure/layout.
    
    Starting from a "fresh" state allows to build the project up with the
    correct structure and design pattern as if there were leftovers from
    the previous repository data resulting in mixed files and folders.
    
    This commit must be pushed first before all other blocked tickets can be
    resolved that are also bound to the epic GH-33!
    See the corresponding milestone (2) for more details about the
    implementation/resolve order.
    
    >>>> Tasks
    
    - `.idea/` (3) - Deleted the whole folder, the files were scoped for
      "Pycharm Community Edition" and will be replaced with the correct
      files for "IntelliJ Ultimate Edition" with the official Go plugin (4)
      (Goland (5)) later on.
    - `assets/` (6) - Deleted the whole folder, all assets will be
      redesigned and added again later on.
    - `bin/` (7) - Deleted the whole folder, the script was part of the
      Python implementation and represented the entry point of the app.
    - `snowsaw/` (8) - Deleted the whole folder, included the main Python
      app and API implementations.
    - `.editorconfig` (9) - Deleted the file, it will be recreated in GH-38
      to match the new project layout and latest "Arctic Ice Studio" project
      design standards/guidelines.
    - `.gitignore` (10) - Deleted the file, it will be recreated in GH-35 to
      match the new project layout and latest "Arctic Ice Studio" project
      design standards/guidelines.
    - `CHANGELOG.md` (11) - Deleted the file, it will be recreated later on to
      match the new project layout and latest "Arctic Ice Studio" project
      design standards/guidelines.
    - `README.md` (12) - Deleted the file, it will be recreated later on to
      match the new project layout and latest "Arctic Ice Studio" project
      design standards/guidelines including the new project assets
      (logo, repository hero etc.).
    
    References:
      (1) https://github.com/arcticicestudio/snowsaw/issues/33
      (2) https://github.com/arcticicestudio/snowsaw/milestone/5
      (3) https://github.com/arcticicestudio/snowsaw/tree/bc54e5136be27f8037de5bbc2f046f37eb036274/.idea
      (4) https://plugins.jetbrains.com/plugin/9568-go
      (5) https://www.jetbrains.com/go
      (6) https://github.com/arcticicestudio/snowsaw/tree/bc54e5136be27f8037de5bbc2f046f37eb036274/assets
      (7) https://github.com/arcticicestudio/snowsaw/tree/bc54e5136be27f8037de5bbc2f046f37eb036274/bin
      (8) https://github.com/arcticicestudio/snowsaw/tree/bc54e5136be27f8037de5bbc2f046f37eb036274/snowsaw
      (9) https://github.com/arcticicestudio/snowsaw/blob/bc54e5136be27f8037de5bbc2f046f37eb036274/.editorconfig
      (10) https://github.com/arcticicestudio/snowsaw/blob/bc54e5136be27f8037de5bbc2f046f37eb036274/.gitignore
      (11) https://github.com/arcticicestudio/snowsaw/blob/bc54e5136be27f8037de5bbc2f046f37eb036274/CHANGELOG.md
      (12) https://github.com/arcticicestudio/snowsaw/blob/bc54e5136be27f8037de5bbc2f046f37eb036274/README.md
    
    Epic: GH-33
    Blocked GH-34 GH-35 GH-36 GH-37 GH-38 GH-39 GH-42 GH-43 GH-44 GH-45
            GH-46 GH-47 GH-48
    Resolves GH-49

--- snowsaw/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .cli import main
-from .plugin import Plugin
--- snowsaw/logging/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-Provides a custom logging class with level color support.
-"""
-from .logger import Logger
-from .color import Color
-from .level import Level
