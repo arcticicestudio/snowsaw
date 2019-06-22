acd0df4f769a739b9c5724b6de2735a59d6f9e40
Arctic Ice Studio <development@arcticicestudio.com>
2019-06-22 21:03:54 +0200

    remark-lint (#54)
    * Install `remark-cli` and `remark-preset-lint-arcticicestudio` NPM pkgs.
    
    Installed remark-lint (1) through `remark-cli` (2) using the
    `remark-preset-lint-arcticicestudio` (3) preset that implements the
    "Arctic ice Studio" Markdown Style Guide (4).
    
    Also updated the Git ignore pattern to handle "Node.js" data.
    
    References:
      (1) remarkjs/remark-lint
      (2) npmjs.com/package/remark-cli
      (3) arcticicestudio/remark-preset-lint-arcticicestudio
      (4) arcticicestudio.github.io/styleguide-markdown
    
    Epic: GH-33
    Depends on GH-47 GH-49
    GH-36
    
    <--------------------------------------------------------------------->
    
    * Implement remark config and ignore pattern file
    
    Created the main remark (1) configuration file and initially set up the
    `remark-preset-lint-arcticicestudio` (2) preset.
    Also added the `.remarkignore` file with to define ignore pattern.
    
    References:
      (1) remark.js.org
      (2) remarkjs/remark-lint
      (3) arcticicestudio/remark-preset-lint-arcticicestudio
    
    Epic: GH-33
    Depends on GH-47 GH-49
    GH-36
    
    <--------------------------------------------------------------------->
    
    * Implement NPM script/task to lint Markdown files
    
    The NPM scripts/tasks allow to run the Markdown linting separately and
    as well as running the all `lint` task together.
    
    Also added the `npm-run-all` package that simplifies the paralell and
    serial execution of multiple NPM scripts/tasks including name wildcard
    support.
    
    Epic: GH-33
    Depends on GH-47 GH-49
    GH-36

---
+++ .remarkrc.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2017-present Sven Greb <development@svengreb.de>
+ *
+ * Project:    snowsaw
+ * Repository: https://github.com/arcticicestudio/snowsaw
+ * License:    MIT
+ */
+
+/**
+ * @file The remark-lint configuration.
+ * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Sven Greb <development@svengreb.de>
+ */
+
+module.exports = {
+  plugins: ["remark-preset-lint-arcticicestudio"]
+};
