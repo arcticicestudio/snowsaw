c68831273e6b1043784789dadcbefb41123cf44e
Arctic Ice Studio <development@arcticicestudio.com>
2019-06-25 20:23:25 +0200

    Internal filesystem utility functions
    The `internal/util` package provides application-wide utility functions.
    The initial implementations are scoped for filesystem actions like
    checking if a file exists at a given path (and is not a directory) as
    well as ensuring absolute paths and necenssary write permissions.
    
    Epic GH-33
    Depends on GH-58
    GH-60

---
+++ internal/util/doc.go
@@ -0,0 +1,13 @@
+// Copyright (C) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
+// Copyright (C) 2017-present Sven Greb <development@svengreb.de>
+//
+// Project:    snowsaw
+// Repository: https://github.com/arcticicestudio/snowsaw
+// License:    MIT
+
+// Author: Arctic Ice Studio <development@arcticicestudio.com>
+// Author: Sven Greb <development@svengreb.de>
+// Since: 0.4.0
+
+// Package util provides application-wide utility functions.
+package util
